a0651197ca3d591b4c91ae5549bcce411bf0642c
ptaushanov <pepi15032000@gmail.com>
2021-10-01 21:55:21 +0300

    feat: added note functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [note, setNote] = useState('');
+
+  const handleTextChange = (e) => setNote(e.target.value)
+  const handleSave = () => localStorage.setItem("note", note);
+  const handleReset = () => {
+    localStorage.removeItem("note")
+    setNote('') 
+  }
+
+  useEffect(()=>{
+    const localStorageNote = localStorage.getItem("note");
+    setNote(localStorageNote)
+  }, [])
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea 
+              className="textarea is-large"
+              placeholder="Notes..." 
+              onChange = { handleTextChange }
+              value = { note }
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button onClick={handleSave} className="button is-large is-primary is-active">Save</button>
+        <button onClick={handleReset} className="button is-large">Clear</button>
       </div>
     </div>
   );
